818324edb54acd8c96f3326afc09ac259ff78c4d
ThMagzuz <markus.dam123@gmail.com>
2019-07-10 11:17:16 +0000

    Restructure

---
+++ loginCommand.py
@@ -0,0 +1,52 @@
+import requests
+import getpass
+import configparser
+import jwTokens
+
+config = configparser.ConfigParser()
+
+config.read('config.ini')
+server_address = config['Server']['server_address']
+
+def loginCommand(args):
+    if args['username']:
+        if not args['password']:
+            password = loginPrompt(True)
+        else:
+            password = args['password']
+        username = args['username']
+    else:
+        username, password = loginPrompt(False)
+    longToken = durationPrompt()
+
+    login(username, password, longToken)
+
+def loginPrompt(usernameGiven):
+    if usernameGiven:
+       return getpass.getpass()
+    else:
+        return input('Username: '), getpass.getpass()
+
+def durationPrompt():
+    duration = input('Would you like your login to be remembered for one day? [y/N] ')
+    return duration == 'y'
+
+def login(username, password, longToken):
+    res = requests.post(server_address + '/users/login', data = {'username': username, 'password': password, 'rememberMe': longToken})
+
+    if res.status_code == 401:
+        print('Invalid login')
+        return False
+    elif res.status_code != 200:
+        print('Something went wrong! Error code ' + res.status_code)
+        return False
+
+    token = res.text[1::][0:-1:]
+
+    with open('token.dat', 'w') as tokenFile:
+        tokenFile.write(token)
+
+    print('Logged in as ' + username)
+
+    return True
+
--- rcsync.py
+++ rcsync.py
@@ -9,57 +9,11 @@ Options:
     --version   Show version
 """
 from docopt import docopt
-import requests
-import getpass
-import configparser
-import jwTokens
+from loginCommand import loginCommand
 
-config = configparser.ConfigParser()
-
-config.read('config.ini')
-server_address = config['Server']['server_address']
-
-def loginCommand():
-    if args['username']:
-        if not args['password']:
-            password = loginPrompt(True)
-        else:
-            password = args['password']
-        username = args['username']
-    else:
-        username, password = loginPrompt(False)
-    longToken = durationPrompt()
-
-    login(username, password, longToken)
-
-def loginPrompt(usernameGiven):
-    if usernameGiven:
-       return getpass.getpass()
-    else:
-        return input('Username: '), getpass.getpass()
-
-def durationPrompt():
-    duration = input('Would you like your login to be remembered for one day? [y/N] ')
-    return duration == 'y'
-
-def login(username, password, longToken):
-    res = requests.post(server_address + '/users/login', data = {'username': username, 'password': password, 'rememberMe': longToken})
-
-    if res.status_code == 401:
-        print('Invalid login')
-        return False
-    elif res.status_code != 200:
-        print('Something went wrong! Error code ' + res.status_code)
-        return False
-
-    token = res.text[1::][0:-1:]
-
-    with open('token.dat', 'w') as tokenFile:
-        tokenFile.write(token)
-
-    print('Logged in as ' + username)
-
-    return True
+commands = {
+    'login': loginCommand
+}
 
 def stripAngleBrackets(string):
     if string[0] == '<':
@@ -67,11 +21,6 @@ def stripAngleBrackets(string):
     if string[-1] == '>':
         string = string[0:-1:]
     return string
-
-commands = {
-    'login': loginCommand
-}
-
 
 args = docopt(__doc__, version='RCSync Client 0.1')
 
@@ -85,7 +34,7 @@ for key in commands.keys():
     # Find the entered command
     if args[key] == True:
         commandFound = True
-        commands[key]()
+        commands[key](args)
 
 if not commandFound:
     print("Unable to find a valid command handler")
